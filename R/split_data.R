126bfba8eed9aee4a1ce76e8281b4a787592424b
angelayandrofski <angelashan084@gmail.com>
2025-04-10 13:29:55 -0700

    Populated the split_data.R

---
+++ R/split_data.R
@@ -0,0 +1,29 @@
+#' Split a Data Frame into Training and Testing Sets by using `initial_split()` from the
+#' `rsample` package (via `tidymodels`) to split a data frame into training and
+#' testing sets.
+#'
+#' @param df A data frame to be split.
+#' @param prop A number between 0 and 1 specifying the proportion of the data to be used for training.
+#' @param strata A string specifying the column name for stratified sampling (must be a column in `df`).
+#' @param seed An integer for reproducibility of the random split.
+#'
+#' @return A list containing two elements:
+#' \describe{
+#'   \item{train}{The training subset of the data.}
+#'   \item{test}{The testing subset of the data.}
+#' }
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+library(tidymodels)
+df <- data.frame(
+   age = c(2, 3, 4, 5, 6, 7, 8, 9),
+   outcome_group = rep(c("Adopted", "Not Adopted"), 4)
+ )
+
+split <- split_data(df, prop = 0.75, strata = "outcome_group", seed = 123)
+ train_data <- split[[1]]
+ test_data <- split[[2]]
+ }
